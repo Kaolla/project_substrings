d4c79f7fa4d97372fc7f574acb2fce05890a8bbf
Kaolla <harlequin.crest@gmail.com>
2016-11-27 21:48:10 +0100

    Improved substring method

--- substrings.rb
+++ substrings.rb
@@ -1,13 +1,6 @@
 def substrings(string, dictionary)
-	substrings = {}
-
-	dictionary.each do |word|
-		count = string.downcase.scan(word).size
-		substrings[word] = count if count != 0
-	end
-	
-	p substrings
+	dictionary.group_by { |word| string.downcase.scan(word).size}
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("Howdy partner, sit down! How's it going?", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
